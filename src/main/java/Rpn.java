434e0de8be9e99aed390a53dae66a66d0dc0b0cf
Sylvain K <sylvain.kocher@avb202.com>
2022-07-11 11:49:21 +0200

    Drop test with getResult, store the stack instead of the expression

--- src/main/java/Rpn.java
+++ src/main/java/Rpn.java
@@ -2,28 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rpn {
-    private final String expression;
+    private final List<Integer> stack = new ArrayList<>();
 
     public Rpn(String expression) {
-        this.expression = expression;
-    }
-
-    public int getResult() {
-        return parseNumber(expression);
-    }
-
-    private int parseNumber(String expression) {
-        return Integer.parseInt(expression);
-    }
-
-    public List<Object> getStack() {
         String[] tokens = expression.split(" ");
-        List<Object> stack = new ArrayList<>();
 
         for (String token : tokens) {
             stack.add(parseNumber(token));
         }
+    }
+    private int parseNumber(String expression) {
+        return Integer.parseInt(expression);
+    }
 
+    public List<Integer> getStack() {
         return stack;
     }
 
--- src/test/java/RpnTest.java
+++ src/test/java/RpnTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -8,20 +7,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class RpnTest {
 
     @Test
-    public void should_a_string_number_containing_a_number_get_the_number() {
-        Rpn rpn = new Rpn("3");
-
-        int result = rpn.getResult();
-
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
     public void should_1_number_be_pushed_in_the_stack() {
         String expression = "3";
         Rpn rpn = new Rpn(expression);
 
-        List<Object> stack = rpn.getStack();
+        List<Integer> stack = rpn.getStack();
 
         List<Object> expected = List.of(3);
         assertThat(stack).isEqualTo(expected);
@@ -32,7 +22,7 @@ public class RpnTest {
         String expression = "3 1";
         Rpn rpn = new Rpn(expression);
 
-        List<Object> stack = rpn.getStack();
+        List<Integer> stack = rpn.getStack();
 
         List<Object> expected = List.of(3, 1);
         assertThat(stack).isEqualTo(expected);
