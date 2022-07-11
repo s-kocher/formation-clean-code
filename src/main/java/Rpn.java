74767347666719a3566e7d81cab8750a5b357c8a
Sylvain K <sylvain.kocher@avb202.com>
2022-07-11 11:59:27 +0200

    4 - sum

--- src/main/java/Rpn.java
+++ src/main/java/Rpn.java
@@ -2,15 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rpn {
+
     private final List<Integer> stack = new ArrayList<>();
 
     public Rpn(String expression) {
         String[] tokens = expression.split(" ");
 
         for (String token : tokens) {
-            stack.add(parseNumber(token));
+            if (token.equals("+")) {
+                int secondElement = popStack();
+                int firstElement = popStack();
+                int sum = firstElement + secondElement;
+                pushStack(sum);
+                continue;
+            }
+            pushStack(parseNumber(token));
         }
     }
+
+    private void pushStack(int number) {
+        stack.add(number);
+    }
+
+    private int popStack() {
+        int topElement = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        return topElement;
+    }
+
     private int parseNumber(String expression) {
         return Integer.parseInt(expression);
     }
--- src/test/java/RpnTest.java
+++ src/test/java/RpnTest.java
@@ -28,4 +28,15 @@ public class RpnTest {
         assertThat(stack).isEqualTo(expected);
     }
 
+    @Test
+    public void should_2_numbers_and_a_plus_be_summed() {
+        String expression = "3 1 +";
+        Rpn rpn = new Rpn(expression);
+
+        List<Integer> stack = rpn.getStack();
+
+        List<Object> expected = List.of(4);
+        assertThat(stack).isEqualTo(expected);
+    }
+
 }
